af3e47de213aabe7a04a6289e824c3665a8814d0
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-11-24 11:17:27 +0700

    Add files via upload

---
+++ hello world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout<<"hello world"<<endl;
+    /*nama saya ikhsan saputra
+    npm 2117051037*/
+    //ikhsan ganteng banget;
+
+
+	return 0;
+}
+
